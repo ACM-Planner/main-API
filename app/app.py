5c05c411cf033da4b1cb7556693ee486cb92ebd5
Patricio López <lopezjuripatricio@gmail.com>
2016-07-27 10:09:39 -0400

    add support

--- app/app.py
+++ app/app.py
@@ -1,5 +1,71 @@
 from flask import Flask, Response, request, jsonify
 
+# Links:
+# https://rdflib.github.io/sparqlwrapper/
+# https://github.com/RDFLib/rdflib
+# https://github.com/RDFLib/rdflib-sparqlstore
+# https://github.com/RDFLib/rdflib-sparqlstore/blob/master/test/test_sparql11.py
+# http://rdflib.readthedocs.io/en/stable/intro_to_sparql.html
+
+# from SPARQLWrapper import SPARQLWrapper, JSON
+#
+# RDF_ENDPOINT = "http://192.168.99.100:3030/test/query"
+#
+# sparql = SPARQLWrapper(RDF_ENDPOINT)
+# sparql.setReturnFormat(JSON)
+#
+# # add a default graph, though that can also be part of the query string
+# # sparql.addDefaultGraph("http://www.example.com/data.rdf")
+#
+# queryString = "SELECT * WHERE { ?s ?p ?o. }"
+# sparql.setQuery(queryString)
+#
+# try:
+#     result = sparql.query().convert()
+#     print(result)
+# except Exception as e:
+#     print(e)
+
+# -----------------------
+
+# from rdflib import ConjunctiveGraph, URIRef
+#
+# graphuri = URIRef('urn:default')
+#
+# graph = ConjunctiveGraph('SPARQLUpdateStore')
+# root = "http://192.168.99.100:3030/test/"
+# graph.open((root + "query", root + "update"))
+#
+# g = graph.get_context(graphuri)
+
+# g = rdflib.ConjunctiveGraph('SPARQLUpdateStore')
+
+# -----------------------
+
+import rdflib
+
+graph = rdflib.ConjunctiveGraph('SPARQLStore')
+graph.open("http://192.168.99.100:3030/test/sparql")
+
+qres = graph.query("""
+    SELECT * WHERE { ?s ?p ?o. }
+""")
+
+# See qres (Result) documentation:
+# http://rdflib.readthedocs.io/en/stable/apidocs/rdflib.html#rdflib.query.Result
+
+# As triples
+for s, p, o in qres:
+    print('----------')
+    print('s:', s)
+    print('p:', p)
+    print('o:', o)
+
+# To JSON
+json_string = qres.serialize(format='json')
+print('JSON:', json_string)
+
+
 app = Flask(__name__)
 
 # In-memory data store
