b74456a946c26c805f07ff5a451d0d8908b3d136
Nebil Kawas <nkawas90@gmail.com>
2016-08-20 12:42:33 -0300

    :books: Added some docstrings to the 'errors' module

--- app/errors.py
+++ app/errors.py
@@ -1,5 +1,16 @@
+"""
+A module for managing errors.
+"""
+
 
 class InvalidUsage(Exception):
+    """
+    Generic class to handle requests' exceptions.
+
+    Adapted from: http://flask.pocoo.org/docs/0.11/patterns/apierrors/
+    ======= =====
+    """
+
     DEFAULT_CODE = 400
 
     def __init__(self, message, payload=None, status_code=None):
